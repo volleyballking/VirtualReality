ad67fb986723fa84d91656a07e51e94db611cb2f
volleyballking <alexl190@nycstudents.net>
2025-10-31 14:57:32 +0000

    2

--- VRLesson2_6Classwork/script.js
+++ VRLesson2_6Classwork/script.js
@@ -1,11 +1,23 @@
 let scene;
+let gridSize = 10;
+let spacing = 15;
 
 window.addEventListener("DOMContentLoaded",function() {
   scene = document.querySelector("a-scene");
   
   mech = document.getElementById("mech-1");
 
-  let clone = mech.cloneNode(true)
+   for (let i = 0; i < gridSize; i++) {
+    for (let o = 0; o < gridSize; o++) {
+      let x = (i - gridSize / 2) * spacing;  
+      let z = (o - gridSize / 2) * spacing;  
+      let y = Math.random() * -3 - 1;   
+      let clone = mech.cloneNode(true);
+      clone.setAttribute("id","mech-"+(i+2));
+      clone.setAttribute("position",{x:x,y:13,z:z});
+      scene.appendChild(clone);     
+    }
+  }
  
   loop();
 })
